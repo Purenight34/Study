8cc76b01e9f1dd09f3f0531d48d0b0f541700494
박예담 <pyd0805@naver.com>
2026-08-20 20:38:20 +0900

    박예담 삽입정렬 문제

--- algorithm/01_sorting/02_insertion_sort/02_insertion_sort.py
+++ algorithm/01_sorting/02_insertion_sort/02_insertion_sort.py
@@ -1,8 +1,8 @@
 import sys
 
 for i in range(3):
-    sys.stdin = open(f"algorithm/01_sorting/02_insertion_sort/tests/input{i+1}.txt", "r")
-    insertion_sort = list(map(int, input().spilt()))
+    sys.stdin = open(f"algorithm/01_sorting/02_insertion_sort/tests/input_0{i+1}.txt", "r")
+    insertion_sort = list(map(int, input().split()))
 
     ######### 여기서부터 작성 
     n = len(insertion_sort)
@@ -13,12 +13,11 @@ for i in range(3):
                 insertion_sort[k],insertion_sort[k-1] = insertion_sort[k-1],insertion_sort[k]
             else : break 
     
-    return insertion_sort
     
     ##########
     print(f"삽입정렬 후 : {insertion_sort}")
-    sys.stdin = open(f"tests/output{i+1}.txt", "r")
-    output_list = list(map(int, input().split()))
+    sys.stdin = open(f"algorithm/01_sorting/02_insertion_sort/tests/output_0{i+1}.txt", "r")
+    output_list = list(map(int,input().split()))
     if insertion_sort == output_list : 
         print(f"Test{i+1} : 정답")
     else:
